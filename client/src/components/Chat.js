68012acb30f3fe7014789525b0d27c9833661f9c
Anabel-Santillan <anabels.20022@gmail.com>
2022-10-14 13:25:21 -0400

    boilerplate , first bug?

---
+++ client/src/components/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => {
+    return (
+    <h1>Chat rendered</h1>
+    )
+}
+
+export default Chat;
---
+++ client/src/components/Join.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Join = () => {
+   return (
+   <h1>Join rendered</h1>
+   )
+}
+
+export default Join;
