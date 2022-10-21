4b157776be0fe267eeee55e96f3b3708fbf5f0a7
nicolasd1323 <nicolasd1323@gmail.com>
2022-10-21 13:31:20 -0400

    new

--- client/src/components/Chat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Chat = () => {
-    return (
-    <h1>Chat rendered</h1>
-    )
-}
-
-export default Chat;
--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//Boiler plate code
-
-import App from './App'
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-
